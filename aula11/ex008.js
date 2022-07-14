e3ae3add123ab51a3574d91f6eb3f425a0d840e3
Tayssa Antoniasse <tayssaantoniasse123@gmail.com>
2022-07-14 19:19:01 -0300

    aprendendo condições simples e compostas usando console.log

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 90
+console.log(`A velocidade do seu carro é ${velocidade}Km/H`)
+if (velocidade > 70) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança :)')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Argentina'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') { //condição composta
+    console.log('Você é Brasileiro(a)!')
+} else {
+    console.log('Você é Estrangeiro(a)')
+}
