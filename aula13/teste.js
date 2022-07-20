f0541afa7b85a3d2688a2d8ebac25d3936912686
Tayssa Antoniasse <tayssaantoniasse123@gmail.com>
2022-07-19 21:01:51 -0300

    aula 13 sobre repetições usando while e do

---
+++ aula13/teste.js
@@ -0,0 +1,16 @@
+//console.log ('Tudo bem?')
+
+
+
+var contador = 1
+while (contador < 224) {
+    console.log(`Passo ${contador}`)
+    contador++                  //cada vez que o comando passa pelo contador é adicionado 1 
+}  //estrutura de repetição com teste lógico no ínicio
+
+
+var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador < 21)  //estrutura de repetição com teste lógico no final
