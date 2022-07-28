78e23ede0bc245c37fb2b8c23bb8b6cead6e660d
Tayssa Antoniasse <tayssaantoniasse123@gmail.com>
2022-07-27 21:36:52 -0300

    função soma usando parametros opcionais n=0

---
+++ aula16/funcao2.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) {
+    return n1+n2
+}
+
+console.log(soma(2)) // ao nao passar um dos parâmetros o código retorna NaN (not a number), para resolver isso é possível colocar o parâmetro como 0 ('opcional')
